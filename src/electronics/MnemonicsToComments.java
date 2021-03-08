62a2981b6af0c2fec6a556064c773651955b61b7
faizansaghir <dbz.rashidsaghir@gmail.com>
2021-03-08 14:41:53 +0530

    Added new mnemonics and improvements made

--- src/electronics/MnemonicsToComments.java
+++ src/electronics/MnemonicsToComments.java
@@ -87,7 +87,7 @@ public class MnemonicsToComments {
 				if(mnemonicString.contains(command)) {
 					
 					String commandToModify=commandsMapping.get(command);
-					System.out.print(mnemonicString+"\t\t");	
+					//System.out.print(mnemonicString+"\t\t");	
 					if(noOfOperand==1) {
 						String operand=getOneOperand(mnemonicString,commandToModify);
 						if(operand.compareTo("stack pointer")==0) {
@@ -163,7 +163,7 @@ public class MnemonicsToComments {
 	public static HashMap<String,String> getOneOperandCommandList(){
 		HashMap<String,String> commandsMap=new HashMap<>();
 		String[] normalStringCommands= {
-			"CMP","INX","DCR","INR","JNZ","JZ","JP","JM","CPI","STA","LDA","ADD","SUB","JPO","ANI","JC","SBB","DAD","JNC","DCX","PUSH","POP"
+			"CMP","INX","DCR","INR","JNZ","JZ","JP","JM","CPI","STA","LDA","ADD","SUB","JPO","ANI","JC","SBB","DAD","JNC","DCX","PUSH","POP","ADC"
 		};
 		String[] commandComments= {
 			"Compare value at accumulator with value at []",
@@ -187,7 +187,8 @@ public class MnemonicsToComments {
 			"If no carry is generated then jump to []",
 			"Decrement [] register pair",
 			"Push value at [] register pair to stack",
-			"Pop value from stack to [] register pair"
+			"Pop value from stack to [] register pair",
+			"Add [] with carry to accumulator"
 		};
 		for(int i=0;i<normalStringCommands.length;i++) {
 			commandsMap.put(normalStringCommands[i],commandComments[i]);
@@ -214,41 +215,140 @@ public class MnemonicsToComments {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String myMnemonics="	   LXI H,2500\r\n" + 
-				"	   MOV C,M\r\n" + 
-				"	   INX H\r\n" + 
-				"	   MOV B,M\r\n" + 
-				"	   DCR C\r\n" + 
-				"	   MOV E,C\r\n" + 
-				"	   MVI A,FF\r\n" + 
-				"	   STA 3000\r\n" + 
-				"\r\n" + 
-				"	   INX H\r\n" + 
-				"	   MOV A,M\r\n" + 
-				"	   CMP B\r\n" + 
-				"	   JM SKIPSWAP\r\n" + 
-				"	   MOV M,B\r\n" + 
-				"	   DCX H\r\n" + 
-				"	   MOV M,A\r\n" + 
-				"	   INX H\r\n" + 
-				"	   MVI A,00\r\n" + 
-				"	   STA 3000\r\n" + 
-				"\r\n" + 
-				"	   MOV B,M\r\n" + 
-				"	   DCR C\r\n" + 
-				"	   JNZ RERUN\r\n" + 
-				"	   LDA 3000\r\n" + 
-				"	   CPI FF\r\n" + 
+		String myMnemonics="	   LDA 2510\r\n" + 
+				"	   CPI 00\r\n" + 
+				"	   JNZ CHECKDENOMINATOR\r\n" + 
+				"	   LDA 2511\r\n" + 
+				"	   CPI 00\r\n" + 
 				"	   JZ END\r\n" + 
-				"	   LXI H,2501\r\n" + 
-				"	   MOV B,M\r\n" + 
-				"	   DCR E\r\n" + 
-				"	   MOV C,E\r\n" + 
-				"	   MVI A,FF\r\n" + 
-				"	   STA 3000\r\n" + 
-				"	   JNZ RERUN\r\n" + 
+				"\r\n" + 
+				"	   LDA 2501\r\n" + 
+				"	   CPI 00\r\n" + 
+				"	   JZ END\r\n" + 
+				"	   STA 3001\r\n" + 
+				"	   STA 3006\r\n" + 
 				"	   MVI A,00\r\n" + 
-				"	   STA 3000";
+				"	   STA 2700\r\n" + 
+				"	   STA 2710\r\n" + 
+				"	   STA 3000\r\n" + 
+				"	   STA 3005\r\n" + 
+				"	   MVI A,01\r\n" + 
+				"	   STA 2701\r\n" + 
+				"	   STA 2711\r\n" + 
+				"\r\n" + 
+				"	   LDA 2700\r\n" + 
+				"	   MOV H,A\r\n" + 
+				"	   LDA 2701\r\n" + 
+				"	   MOV L,A\r\n" + 
+				"	   DAD H\r\n" + 
+				"	   MOV A,H\r\n" + 
+				"	   STA 2700\r\n" + 
+				"	   MOV A,L\r\n" + 
+				"	   STA 2701\r\n" + 
+				"	   LDA 3000\r\n" + 
+				"	   MOV H,A\r\n" + 
+				"	   LDA 3001\r\n" + 
+				"	   MOV L,A\r\n" + 
+				"	   DAD H\r\n" + 
+				"	   MOV A,H\r\n" + 
+				"	   STA 3000\r\n" + 
+				"	   MOV A,L\r\n" + 
+				"	   STA 3001\r\n" + 
+				"	   LDA 2510\r\n" + 
+				"	   CMP H\r\n" + 
+				"	   JZ CHECKLOWER\r\n" + 
+				"	   JP STORE\r\n" + 
+				"	   JM CALCULATE\r\n" + 
+				"\r\n" + 
+				"                       LDA 2511\r\n" + 
+				"	   CMP L\r\n" + 
+				"	   JZ FINALSTORE\r\n" + 
+				"	   JM CNF\r\n" + 
+				"	   JC CALCULATE\r\n" + 
+				"\r\n" + 
+				"	   LDA 3000\r\n" + 
+				"	   STA 3005\r\n" + 
+				"	   LDA 3001\r\n" + 
+				"	   STA 3006\r\n" + 
+				"	   LDA 2700\r\n" + 
+				"	   STA 2710\r\n" + 
+				"	   LDA 2701\r\n" + 
+				"	   STA 2711\r\n" + 
+				"	   JMP MAXIMIZE\r\n" + 
+				"\r\n" + 
+				"	   LDA 3000\r\n" + 
+				"	   STA 3005\r\n" + 
+				"	   LDA 3001\r\n" + 
+				"	   STA 3006\r\n" + 
+				"	   LDA 2700\r\n" + 
+				"	   STA 2710\r\n" + 
+				"	   LDA 2701\r\n" + 
+				"	   STA 2711\r\n" + 
+				"\r\n" + 
+				"	   LDA 2510\r\n" + 
+				"	   MOV H,A\r\n" + 
+				"	   LDA 2511\r\n" + 
+				"	   MOV L,A\r\n" + 
+				"	   LDA 3005\r\n" + 
+				"	   MOV B,A\r\n" + 
+				"	   LDA 3006\r\n" + 
+				"	   MOV C,A\r\n" + 
+				"	   MOV A,L\r\n" + 
+				"	   SUB C\r\n" + 
+				"	   JZ UNSETCARRY\r\n" + 
+				"	   JP UNSETCARRY\r\n" + 
+				"	   JMP AFTERUNSET\r\n" + 
+				"\r\n" + 
+				"	   STC\r\n" + 
+				"	   CMC\r\n" + 
+				"\r\n" + 
+				"	   MOV L,A\r\n" + 
+				"	   MOV A,H\r\n" + 
+				"	   SBB B\r\n" + 
+				"	   MOV H,A\r\n" + 
+				"	   JZ BEFOREROTATE\r\n" + 
+				"	   JM ROTATE\r\n" + 
+				"\r\n" + 
+				"	   MOV A,H\r\n" + 
+				"	   STA 2510\r\n" + 
+				"	   MOV A,L\r\n" + 
+				"	   STA 2511\r\n" + 
+				"	   LDA 4001\r\n" + 
+				"	   LXI H,2711\r\n" + 
+				"	   ADD M\r\n" + 
+				"	   STA 4001\r\n" + 
+				"	   LDA 4000\r\n" + 
+				"	   LXI H,2710\r\n" + 
+				"	   ADC M\r\n" + 
+				"	   STA 4000\r\n" + 
+				"\r\n" + 
+				"	   LDA 2711\r\n" + 
+				"	   CPI 01\r\n" + 
+				"	   JZ END\r\n" + 
+				"	   LDA 2510\r\n" + 
+				"	   CPI 00H\r\n" + 
+				"	   JZ CHECK\r\n" + 
+				"	   JMP NUMNOTZERO\r\n" + 
+				"	   LDA 2511\r\n" + 
+				"	   CPI 00H\r\n" + 
+				"	   JZ END\r\n" + 
+				"	   LDA 3005\r\n" + 
+				"	   STC\r\n" + 
+				"	   CMC\r\n" + 
+				"	   RAR\r\n" + 
+				"	   STA 3005\r\n" + 
+				"	   LDA 3006\r\n" + 
+				"	   RAR\r\n" + 
+				"	   STA 3006\r\n" + 
+				"	   LDA 2710\r\n" + 
+				"	   STC\r\n" + 
+				"	   CMC\r\n" + 
+				"	   RAR\r\n" + 
+				"	   STA 2710\r\n" + 
+				"	   LDA 2711\r\n" + 
+				"	   RAR\r\n" + 
+				"	   STA 2711\r\n" + 
+				"	   JMP CALCULATE\r\n";
 		commentAll(myMnemonics);
 	}
 
